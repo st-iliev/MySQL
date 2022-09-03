c8243082cffaba88138397db6a21a50db8d38992
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-09-03 23:58:47 +0300

    Exercise

---
+++ Data Definition And Data Types/Exercise/Create Tables.sql
@@ -0,0 +1,10 @@
+create table minions(
+`id` INT NOT NULL AUTO_INCREMENT primary key,
+`name` varchar(50) NOT NULL,
+`age` INT NOT NULL
+);
+create table towns(
+`town_id` INT NOT NULL  AUTO_INCREMENT primary key,
+`name` varchar(50) NOT NULL
+);
+
